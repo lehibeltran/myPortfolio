16c7aaf31b9e453a0da59fbb7bfeb8fc5f43c39a
lehibeltran86 <lehibeltran@hotmail.com>
2016-07-19 11:46:55 -0600

    final touches to main page

--- js/bodyDir.js
+++ js/bodyDir.js
@@ -3,9 +3,6 @@ angular.module('myApp').directive('bodyDir', function(){
         templateUrl: '../views/body.html',
         restrict: 'E',
         link: function(scope, elem, attrs){
-            // elem.bind('click', function(e) {
-            //     scope.imageValue = $(this).find('img').attr('src');
-            // });
             scope.closing = function(){
                 scope.imageValue = "";
                 scope.modalTitle = "";
