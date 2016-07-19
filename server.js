a51939225b85231d4217597102ee143f20fde4e6
lehibeltran86 <lehibeltran@hotmail.com>
2016-07-19 14:31:07 -0600

    server config added

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
  * Created by lehi on 7/19/16.
  */
 var express = require('express');
+var config = require('./config.js');
+var port = config.portNum;
 var app = express();
 
 app.use(express.static('public'));
 app.use('/packages', express.static(__dirname + '/node_modules/'));
 
-var port = 3000;
 app.listen(port, function() {
-    console.log('I\'m listening on port: ', port);
+    console.log('Making tacos at: ', port);
 });
